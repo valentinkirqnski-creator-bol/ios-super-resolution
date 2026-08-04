ac0ff06b7f2eb5a85896789ede3a2f4e1ba80bc8
Valentin <valentink373@gmail.com>
2026-08-04 13:52:11 +0300

    Refine alignment at each pyramid level

--- core/align.cpp
+++ core/align.cpp
@@ -834,15 +834,16 @@ FlowField align(const Pyramid& ref_pyr, const Image& ref_grey,
         else
             block_match_level_L2(r, m, ts, radius, flow, cfg.num_threads);
 
+        Image gx = compute_sobel_gradx(r);
+        Image gy = compute_sobel_grady(r);
+        HessianField hess = compute_hessian(gx, gy, ts);
+        if (lvl == 0) {
+            debug_dump_bin("cpp_gradx_ica", gx.data.data(), gx.data.size());
+            debug_dump_bin("cpp_grady_ica", gy.data.data(), gy.data.size());
+        }
+        ica_refine_level(r, gx, gy, m, hess, flow, ts,
+                         cfg.ica_n_iter, cfg.num_threads);
     }
-
-    Image gx = compute_sobel_gradx(ref_grey);
-    Image gy = compute_sobel_grady(ref_grey);
-    HessianField hess = compute_hessian(gx, gy, tile_size);
-    debug_dump_bin("cpp_gradx_ica", gx.data.data(), gx.data.size());
-    debug_dump_bin("cpp_grady_ica", gy.data.data(), gy.data.size());
-    ica_refine_level(ref_grey, gx, gy, moving_grey, hess, flow, tile_size,
-                     cfg.ica_n_iter, cfg.num_threads);
     return flow;
 }
 
